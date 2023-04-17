144c239272d41da9aad8449bf4a0e3300a882906
KidChoque <lucassmachado1998@gmail.com>
2023-04-17 11:00:13 -0300

    Iniciando projeto

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿// Criar uma aplicação para uma agência de turismo, no qual deveremos registrar passagens aéreas com os seguintes dados: Nome do passageiro, Origem, Destino e Data do Voo de 5 passageiros.
+
+// Antes de entrar no sistema faça um esquema do qual o usuário só possa acessar o menu se a senha for igual à 123456.
+// O sistema deve exibir um menu com as seguintes opções:
+
+// 1- Cadastrar passagem
+// 2- Listar Passagens
+// 0- Sair
+
+
+
+
+
+Console.WriteLine(@$"
+
+Seja bem vindo a Lucas Airlines
+
+");
+
+
+Console.Write(@$"Digite a senha para acessar o menu:");
+
+string senha = Console.ReadLine();
+
+while( senha != "1234" )
+{
+ Console.Write(@$"Senha incorreta.Digite Novamente:");
+ senha = Console.ReadLine();
+  
+}
+
+Console.WriteLine(@$"
+
+
+Bem vindo ao menu
+
+
+");
+
+
+
+
